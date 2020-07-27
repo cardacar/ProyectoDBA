7605c7f0749b31105ff0d36fca8a18164bafa1cc
Carlos Quiros <carlosquiros12@gmail.com>
2020-07-26 21:35:27 -0500

    Se modifico la visto del fragment Create

---
+++ app/src/main/java/com/personal/proyectodba/model/producto.java
@@ -0,0 +1,50 @@
+package com.personal.proyectodba.model;
+
+public class producto {
+
+    private int codigo;
+    private String nombre;
+    private String categoria;
+    private String precio;
+
+    public producto() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+}
