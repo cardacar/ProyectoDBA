e70e515fe82556995c301bd0622f50cd984f5743
Carlos Quiros <carlosquiros12@gmail.com>
2020-07-31 14:21:13 -0500

    Cambios minimos a la clase Pedido

--- app/src/main/java/com/personal/proyectodba/model/Pedido.java
+++ app/src/main/java/com/personal/proyectodba/model/Pedido.java
@@ -1,5 +1,13 @@
 package com.personal.proyectodba.model;
 
+import java.util.ArrayList;
+
 public class Pedido {
+    private String nombre;
+    private String telefono;
+    private String direccion;
+    private String codigo;
+    private ArrayList<producto> productList;
+
 
 }
