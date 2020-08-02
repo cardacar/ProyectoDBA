9a1cc1aeeb39e5b2160d7c79fa6bb2674f4ba121
Carlos Quiros <carlosquiros12@gmail.com>
2020-08-01 20:56:06 -0500

    a

--- app/src/main/java/com/personal/proyectodba/PedidoOBuscar.java
+++ app/src/main/java/com/personal/proyectodba/PedidoOBuscar.java
@@ -2,17 +2,44 @@ package com.personal.proyectodba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class PedidoOBuscar extends AppCompatActivity {
+
+    private  Button order,orderFind,productsAvailable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido_o_buscar);
 
-        Button pedir,orderFind,disponibilidad;
+       order = (Button) findViewById(R.id.btnPedir);
+       orderFind = (Button) findViewById(R.id.btnorderFind);
+       productsAvailable = (Button) findViewById(R.id.btnProductosDisponibles);
+
+       order.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(PedidoOBuscar.this,Order.class));
+           }
+       });
+
+       orderFind.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(PedidoOBuscar.this,OrderFind.class));
+           }
+       });
+
+       productsAvailable.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               startActivity(new Intent(PedidoOBuscar.this,ProductsAvailable.class));
+           }
+       });
 
 
 
