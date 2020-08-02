f0d57fe9a2bf57638fdf62029ec1e67e438a6c42
Carlos Quiros <carlosquiros12@gmail.com>
2020-08-01 20:24:27 -0500

    Creando el menu de usuario de forma de activities

---
+++ app/src/main/java/com/personal/proyectodba/PedidoOBuscar.java
@@ -0,0 +1,20 @@
+package com.personal.proyectodba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class PedidoOBuscar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido_o_buscar);
+
+        Button pedir,orderFind,disponibilidad;
+
+
+
+    }
+}
---
+++ app/src/main/java/com/personal/proyectodba/PrductosAvailable.java
@@ -0,0 +1,14 @@
+package com.personal.proyectodba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrductosAvailable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prductos_available);
+    }
+}
