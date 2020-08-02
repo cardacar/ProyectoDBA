d915194ace6616a4ed3d225f1bc7f12fb73888eb
Carlos Quiros <carlosquiros12@gmail.com>
2020-08-01 19:17:22 -0500

    Se completo la clase de pedido y se agrego un boton al principio

--- app/src/main/java/com/personal/proyectodba/model/Pedido.java
+++ app/src/main/java/com/personal/proyectodba/model/Pedido.java
@@ -9,5 +9,55 @@ public class Pedido {
     private String codigo;
     private ArrayList<producto> productList;
 
+    public Pedido() {
 
+    }
+
+    public Pedido(String nombre, String telefono, String direccion, String codigo, ArrayList<producto> productList) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.codigo = codigo;
+        this.productList = productList;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public ArrayList<producto> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(ArrayList<producto> productList) {
+        this.productList = productList;
+    }
 }
